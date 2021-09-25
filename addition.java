5be0ca716fce21e7a9ea727975d60c6ec3237a18
devopslab2021 <devopslab2021@gmail.com>
2021-09-25 18:38:12 +0530

    GitJen Commit

---
+++ addition.java
@@ -0,0 +1,19 @@
+class addition
+{
+public static void main(String abc[])
+ {
+int a,b;
+
+  a = 11;
+  b = 12;
+
+if(a >= 10 && b>= 10)
+  {
+	System.out.println("The Addition Of Two Number's Is: " +(a+b));
+  }
+else
+  {
+	System.out.println("Addition Cannot Be Done. Because Number's Are Not Greater Than 10.");
+  }
+ }
+}
